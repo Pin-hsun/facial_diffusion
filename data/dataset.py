4778956e4eb59ca650cb16ea583c786a720faa77
gloria <a24621391@gmail.com>
2024-05-17 12:59:23 +0800

    240517

--- data/dataset.py
+++ data/dataset.py
@@ -197,8 +197,9 @@ class ColorizationDataset(data.Dataset):
         return len(self.flist)
 
 class InpaintAUDataset(data.Dataset):
-    def __init__(self, data_root, GT, condition, landmark, mask_config={}, data_len=-1, image_size=256, loader=pil_loader):
-        imgs = make_dataset(os.path.join(data_root, GT))
+    def __init__(self, dataset, data_root, GT, condition, landmark, mask_config={},
+                 data_len=-1, image_size=256, loader=pil_loader):
+        imgs = make_dataset(os.path.join(data_root, GT, "images"))
         cond_imgs = make_dataset(os.path.join(data_root, condition))
         lds = make_landmark_dataset(os.path.join(data_root, landmark,'landmark'))
 
@@ -213,16 +214,14 @@ class InpaintAUDataset(data.Dataset):
             self.lds = lds
             self.cond_imgs = cond_imgs
         self.tfs = transforms.Compose([
-                transforms.CenterCrop((min(one_img.size), min(one_img.size))), #for celebA dataset
                 transforms.Resize((image_size, image_size)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
         ])
         self.ld_tfs = transforms.Compose([
                 transforms.ToPILImage(),
-                transforms.CenterCrop((min(one_img.size), min(one_img.size))), #for celebA dataset
                 transforms.Resize((image_size, image_size)),
-                transforms.ToTensor()
+                transforms.ToTensor(),
         ])
         self.loader = loader
         self.mask_config = mask_config
@@ -230,6 +229,8 @@ class InpaintAUDataset(data.Dataset):
         self.image_size = [image_size, image_size]
         self.ori_image_size = one_img.size
         self.condition = condition
+        self.dataset = dataset
+        assert self.dataset in ['CelebA', 'CelebAHQmask', 'AffectNet']
 
     def __getitem__(self, index):
         ret = {}
@@ -261,14 +262,24 @@ class InpaintAUDataset(data.Dataset):
                                                           max_bbox_shape=(self.image_size[0]//2, self.image_size[1]//2)))
         elif self.mask_mode == 'center':
             h, w = self.image_size
-            mask = bbox2mask(self.image_size, (h // 4 + 20, w // 4, h // 2 , w // 2))
+            if self.dataset == 'AffectNet':
+                start, length = int(h//7), int(h//1.4)
+                mask = bbox2mask(self.image_size, (start, start, length, length))
+            elif self.dataset == 'CelebA' or self.dataset == 'CelebAHQmask':
+                start, length = int(h//4), int(h//2)
+                mask = bbox2mask(self.image_size, (start + 20, start, length , length))
         elif self.mask_mode == 'irregular':
             mask = get_irregular_mask(self.image_size)
         elif self.mask_mode == 'free_form':
             mask = brush_stroke_mask(self.image_size)
         elif self.mask_mode == 'hybrid':
             h, w = self.image_size
-            regular_mask = bbox2mask(self.image_size, (h // 4 + 20, w // 4, h // 2 , w // 2))
+            if self.dataset == 'AffectNet':
+                start, length = int(h//7), int(h//1.4)
+                regular_mask = bbox2mask(self.image_size, (start, start, length, length))
+            elif self.dataset == 'CelebA' or self.dataset == 'CelebAHQmask':
+                start, length = int(h//4), int(h//2)
+                regular_mask = bbox2mask(self.image_size, (start + 20, start, length , length))
             irregular_mask = brush_stroke_mask(self.image_size, )
             mask = regular_mask | irregular_mask
         elif self.mask_mode == 'au':
@@ -284,10 +295,14 @@ class InpaintAUDataset(data.Dataset):
 if __name__ == "__main__":
     import sys
     sys.path.append('/home/glory/projects/Palette-Image-to-Image-Diffusion-Models')
-    dataset = InpaintAUDataset(data_root="/home/glory/projects/NTU_Parkinson_Project/results/CelebA/240418_acgan_ep90/XY", mask_config={'mask_mode':'au'})
+    dataset = InpaintAUDataset(dataset="AffectNet" ,data_root="/media/ziyi/glory/AffectNet/Happiness/test", GT="GT",
+                    condition="transformed", landmark="transformed", image_size= 256, mask_config={'mask_mode':'center'})
     print(len(dataset))
-    for i in range(500):
+    for i in range(10):
         mask_img = dataset[i]['mask_image'].permute(1,2,0).numpy()
+        cond_img = dataset[i]['cond_image'].permute(1,2,0).numpy()
         mask_img = (mask_img + 1) / 2
         plt.imsave('tmp/' + str(i) + 'mask.jpg', mask_img)
+        cond_img = (cond_img + 1) / 2
+        plt.imsave('tmp/' + str(i) + 'cond.jpg', cond_img)
         plt.close()
